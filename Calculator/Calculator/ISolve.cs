fe8504fdcc92bcfd2cd92712aacaf252c1571f80
esc5000 <94329037+esc5000@users.noreply.github.com>
2021-11-14 18:24:19 -0600

    Add files via upload

---
+++ Calculator/Calculator/ISolve.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface ISolve
+    {
+        void Accumulate(string s);
+
+        void Clear();
+
+        double Solve();
+    }
+}
